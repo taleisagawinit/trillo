c5860ae930a9fbc2af422d98af73ab96a9f8ffad
taleipono <taleipono@gmail.com>
2020-09-23 01:54:53 -0700

    adds image gallery

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './app.scss';
-import Navigation from './layout/navigation/Navigation';
-import Header from './layout/header/Header';
+// import Navigation from './layout/navigation/Navigation';
+// import Header from './layout/header/Header';
+import ImageGallery from './components/image-gallery/ImageGallery';
 
 
 function App() {
   return (
-    <div style={{"position": "relative"}}>
-      <Header />
-      <Navigation />
+    <div>
+      <ImageGallery
+        alt1="img 1"
+        img1="/img/hotel-1.jpg"
+        alt2="img 2"
+        img2="/img/hotel-2.jpg"
+        alt3="img 3"
+        img3="/img/hotel-3.jpg"
+       />
     </div>
   );
 }
---
+++ src/components/image-gallery/ImageGallery.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function(props) {
+  return (
+    <div className="image-gallery">
+      <img className="image-gallery__img" alt={props.alt1} src={props.img1}></img>
+      <img className="image-gallery__img" alt={props.alt2} src={props.img2}></img>
+      <img className="image-gallery__img" alt={props.alt3} src={props.img3}></img>
+    </div>
+  )
+}
